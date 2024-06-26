5c301fcb291b4c819b7a10dff1ed01d402ed5a25
Sourav Das <souravdas360@gmail.com>
2024-06-26 13:45:45 +0530

    implemented auth middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
 dotenv.config();
+import cookieParser from "cookie-parser";
+
 const port = process.env.PORT || 5000;
 import userRoutes from "./routes/userRoutes.js";
 
@@ -14,6 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
